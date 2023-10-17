5509969763ed8f297498367dde9239c9a8bc67b4
Adho <a.galgalo@platcorpgroup.com>
2023-10-17 08:54:27 +0300

    chore: setup credentials for mysql

--- Code/app.py
+++ Code/app.py
@@ -3,9 +3,9 @@ import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='app_test',
+                             user='devops04',
+                             password='chukula@adho13',
+                             db='ansible',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
